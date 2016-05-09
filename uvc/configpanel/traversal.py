556d312a38e878beae8d77b574f9cffe5e19764a
Souheil CHELFOUH <trollfot@gmail.com>
2016-05-09 16:51:55 +0200

    Added more pluggability

--- uvc/configpanel/traversal.py
+++ uvc/configpanel/traversal.py
@@ -4,7 +4,6 @@ import grok
 import uvcsite
 import datetime
 
-
 from BTrees.OOBTree import OOBTree
 from zope.interface import Interface
 from zope.component import getUtility
@@ -13,14 +12,6 @@ from zope.location import Location, LocationProxy
 from zope.traversing.interfaces import ITraversable
 from zope.dublincore.interfaces import IDCDescriptiveProperties
 from .interfaces import IConfigurator, IConfigurablePlugin, IPluginConfiguration
-
-
-def get_config(name):
-    homefolder = uvcsite.getHomeFolder(uvcsite.getRequest())
-    config = homefolder.get('__config__', None)
-    if config:
-        return config.get(name)
-    return
 
 
 class Configurator(OOBTree):
@@ -33,6 +24,30 @@ class Configuration(PersistentDict, Location):
     grok.implements(IPluginConfiguration)
 
 
+def get_config(name):
+    homefolder = uvcsite.getHomeFolder(uvcsite.getRequest())
+    config = homefolder.get('__config__', None)
+    if config:
+        return config.get(name)
+    return
+
+
+def get_plugin_configuration(homefolder=None, request=None, name=None):
+    if homefolder is None:
+        if request is None:
+            request = uvcsite.getRequest()
+        homefolder = uvcsite.getHomeFolder(request)
+    if '__config__' not in homefolder:
+        homefolder['__config__'] = Configurator()
+    configurator = homefolder['__config__']
+    if not name:
+        return configurator
+    elif name not in configurator:
+        config_item = getUtility(IConfigurablePlugin, name=name)
+        configurator[name] = config_item()
+    return LocationProxy(configurator[name], configurator, name)
+
+
 class PluginConfigurationNamespace(grok.MultiAdapter):
     grok.name('plugins')
     grok.provides(ITraversable)
@@ -43,13 +58,4 @@ class PluginConfigurationNamespace(grok.MultiAdapter):
         self.request = request
 
     def traverse(self, name, ignore):
-        homefolder = uvcsite.getHomeFolder(self.request)
-        if '__config__' not in homefolder:
-            homefolder['__config__'] = Configurator()
-        configurator = homefolder['__config__']
-        if not name:
-            return configurator
-        elif name not in configurator:
-            config_item = getUtility(IConfigurablePlugin, name=name)
-            configurator[name] = config_item()
-        return LocationProxy(configurator[name], configurator, name)
+        return get_plugin_configuration(request=self.request, name=name)
