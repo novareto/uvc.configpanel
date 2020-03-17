3ea8cd64dc0a31ee7ff7c54b9beac347cb549e93
Souheil CHELFOUH <trollfot@gmail.com>
2020-03-17 15:26:17 +0100

    protecting view

--- uvc/configpanel/config.py
+++ uvc/configpanel/config.py
@@ -11,7 +11,7 @@ import uvcsite.browser.layout.slots.interfaces
 class MyPrefsMenu(uvcsite.browser.layout.menu.MenuItem):
     grok.name("my_prefs_menu")
     grok.title(u"Erweiterte Einstellungen")
-    #grok.require('uvc.ManageCoUsers')
+    grok.require('uvc.ManageCoUsers')
     uvcsite.browser.layout.menu.menu(
         uvcsite.browser.layout.slots.interfaces.IPersonalMenu
     )
